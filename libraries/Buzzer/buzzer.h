313bc3240ff631c3cf8de708b3817559ba370e47
Unknown <kam.grunwald@gmail.com>
2019-01-31 16:47:08 +0000

    Buzzer v0.2

--- libraries/Buzzer/buzzer.h
+++ libraries/Buzzer/buzzer.h
@@ -1,17 +1,47 @@
+
+enum play_commands{detectionLeft, detectionRight, detectionBoth, doNothing};
+
 class buzzer{
 protected:
-	int tone0, tone1, tone2, tone3;
+	int reset = -1;
+	int tone0 = 261;
+	int tone1 = 523;
+	int tone2 = 466;
+	int tone3 = 349;
+	int duration;
 	bool enabled;
+	
 public:
-	setup(int buzzerPin){
-	//set up tone values and pin mode as well as initialize flag
-		int tone0 = 261;
-		int tone1 = 523;
-		int tone2 = 466;
-		int tone3 = 349;
+	buzzer_setup(){ enabled = false; }
 	
+	buzzer_setup(int buzzerPin){
+	//set up pin mode as well as initialize flag
 		pinMode(buzzerPin, OUTPUT);
+		enabled = true;	
+	}
 	
+	play_tone(outTone){
+		tone(buzzerPin, outTone, duration)
+	}
+	
+	detection_signal(play_commands detection){
+		switch(detection){
+			case detectionLeft:
+				play_tone(tone1);
+				play_tone(tone0);				
+				play_tone(reset);
+			case detectionRight:
+				play_tone(tone2);
+				play_tone(tone0);
+				play_tone(reset);
+			case detectionBoth:
+				play_tone(tone3);
+				play_tone(tone0);
+				play_tone(reset);
+			case doNothing:
+				play_tone(reset);
+		}
+		
 	}
 
 };
