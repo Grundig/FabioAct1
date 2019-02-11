fa3e59d41b4a5063bbaa4469af58aef8426a6b8f
Unknown <kam.grunwald@gmail.com>
2019-02-11 09:53:55 +0000

    run sketch

--- libraries/System/the_system.h
+++ libraries/System/the_system.h
@@ -6,7 +6,7 @@
 #include "..\Controller\Controller.h" //Joshua's function
 #include "..\he_sensor\he_sensor.h" //Gabriel's function
 #endif
-class system{
+class the_system{
 protected:
 	buzzer action_unit;
 	controller control_unit;
@@ -17,24 +17,27 @@ protected:
 		}
 
 public:
+	the_system(){  bool all_enabled = check_all_enabled();	}
+	//default constructor
+	
 	void setup_sensing(int right_in_pin, int left_in_pin){
 		sensing_unit.setup_he_sensor(right_in_pin, left_in_pin);
-	}
+	} //runs the setup from sensing unit
 
 	void setup_action(int buzzer_pin){
 		action_unit.setup_buzzer(buzzer_pin);
-	}
+	} //runs the setup from action unit
 
 	void setup_control(int in_delta_input, int in_check_interval_ms){
 		control_unit.setup_controller(in_check_interval_ms);
-	}
+	} //runs the setup from controller unit
 
 	// this is the main function that the system performs
 	void execute_system_task(){
 		if(control_unit.is_time_to_take_measurement()){
 			
 			control_unit.issueCommand();
-			//runs the 
+			//runs the main function from the controll
 			
 			
 		}
