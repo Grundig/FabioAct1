ff8b9a91de650d1a5cb7ba43284aadfa57832f4f
dragonulera <47215407+dragonulera@users.noreply.github.com>
2019-02-11 11:06:29 +0000

    Update system_run_sketch.ino

--- sketches/system_run_sketch/system_run_sketch.ino
+++ sketches/system_run_sketch/system_run_sketch.ino
@@ -3,8 +3,8 @@
 //#include <he_sensor.h>
 //#include <Controller.h>
 the_system system_unit;
-int right_in_pin = A1;
-int left_in_pin = A2;
+int right_in_pin = A0;
+int left_in_pin = A1;
 void setup() {
   // put your setup code here, to run once:
 //  int right_in_pin = A1;
@@ -23,14 +23,19 @@ void setup() {
 
 void loop() {
   
+  int inp1, inp2;
+    
+    inp1=analogRead(right_in_pin);
+    inp2=analogRead(left_in_pin);
   
   // put your main code here, to run repeatedly:
   Serial.println("Input_1:");
 
-    Serial.println(right_in_pin);
+    Serial.println(inp1);
 
     Serial.println("Input_2:");
 
-    Serial.println(left_in_pin);
+    Serial.println(inp2);
   system_unit.execute_system_task();
+  delay(2000);
 }
